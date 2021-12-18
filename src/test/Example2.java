222ec6b60a4762bbcfcf0b414e0c3ebcdd7e8f31
yogendra <yogendra@DESKTOP-KIS4N2E>
2021-12-18 11:08:40 +0530

    Made Changes

---
+++ src/test/Example2.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Example2 
+{
+	public static void method1()
+	{
+		System.out.println("Hi Method1 is running");	
+	}
+	
+	public static void method2()
+	{
+		System.out.println("Hi Method2 is running");
+	}
+
+}
